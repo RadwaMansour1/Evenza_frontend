3aaa27e8483e1df6ba319fb25c87dadfffe0d398
Asmaa omar <119797320+AsmaaOmarr@users.noreply.github.com>
2025-04-19 14:59:43 +0200

    feat: add ngx-translate for multilingual support and create language switcher

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TranslateModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'evenza-frontend';
+  constructor(private translate: TranslateService) {}
+  changeLanguage() {
+    let currentLang = this.translate.currentLang;
+    this.translate.use(currentLang === 'en' ? 'ar' : 'en');
+  }
 }
