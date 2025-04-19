8696026969256c702a8e400f9f0ab2645b532962
Asmaa omar <119797320+AsmaaOmarr@users.noreply.github.com>
2025-04-20 01:27:36 +0200

    feat: add footer component with social links and update nav-bar styles; integrate feather icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { NavBarComponent } from "./components/nav-bar/nav-bar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TranslateModule, NavBarComponent],
+  imports: [RouterOutlet, TranslateModule, NavBarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  
+
 })
 export class AppComponent {
   title = 'evenza-frontend';
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { NgIcon,provideIcons } from '@ng-icons/core';
+import {
+  featherFacebook,
+  featherTwitter,
+  featherInstagram,
+  featherLinkedin,
+  featherCalendar,
+} from '@ng-icons/feather-icons';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  providers: [
+    provideIcons({
+      featherFacebook,
+      featherTwitter,
+      featherInstagram,
+      featherLinkedin,
+      featherCalendar,
+    }),
+  ],
 })
-export class FooterComponent {
-
-}
+export class FooterComponent {}
