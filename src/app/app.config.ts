36b6e172cd2ede1bee4d415a0ddd46d17dde9b33
Hassannzidan <hzidan014@gmail.com>
2025-04-23 20:04:39 +0200

    'edit app config.ts without adding authInterceptor'

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,6 +12,8 @@ import {
 } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideIcons } from '@ng-icons/core';
+import { heroEnvelope, heroLockClosed, heroUser } from '@ng-icons/heroicons/outline';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -31,6 +33,11 @@ export const appConfig: ApplicationConfig = {
     ),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
+    provideIcons({
+      heroEnvelope,
+      heroUser,
+      heroLockClosed  
+    })
     // provideStore(appStore),
     // provideEffects([EventEffects]),
   ],
