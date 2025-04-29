736bd0a1cda5974164f39f56aed1ab67ee9450d5
Hassannzidan <hzidan014@gmail.com>
2025-04-29 16:06:53 +0300

    'solve conflicts'

--- src/app/models/profile.model.ts
+++ src/app/models/profile.model.ts
@@ -35,16 +35,3 @@ export interface ProfileResponse {
 
 
 
-    profileImage?: File;
-    firstName: string;
-    lastName: string;
-    phone1: string;
-    phone2?: string;
-    email?: string;
-    gender: string;
-    country: string;
-    address: string;
-    city: string;
-    state?: string;
-    zipCode?: string;
-}
