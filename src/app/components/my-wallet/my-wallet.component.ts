6f496d3fc9e5f18ed559aad847d52ed7fc586d0b
“Ahmed <ahmedmostafa4497@gmail.com>
2025-04-29 23:18:59 +0300

    update wallet component with actual data

--- src/app/components/my-wallet/my-wallet.component.ts
+++ src/app/components/my-wallet/my-wallet.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { heroArrowDown, heroArrowUp, heroWallet  } from '@ng-icons/heroicons/outline';
+import { WalletService } from '../../services/wallet/wallet.service';
+import { UserService } from '../../services/profile/user.service';
 
 @Component({
   selector: 'app-my-wallet',
@@ -9,26 +11,37 @@ import { heroArrowDown, heroArrowUp, heroWallet  } from '@ng-icons/heroicons/out
   templateUrl: './my-wallet.component.html',
   viewProviders: [provideIcons({ heroArrowDown, heroArrowUp, heroWallet })],
 })
-export class MyWalletComponent {
-  balance: number = 250.00;
+export class MyWalletComponent implements OnInit{
+  balance: number = 0;
   currency: string = 'EGP';
-  lastTransaction: string = 'April 15, 2025';
-  transactions: Array<any> = [
-    {
-      id: 'T001',
-      type: 'purchase',
-      amount: -99.00,
-      description: 'Summer Music Festival Ticket',
-      date: 'April 10, 2025'
-    },
-    {
-      id: 'T002',
-      type: 'refund',
-      amount: +50.00,
-      description: 'Partial Refund - Tech Conference',
-      date: 'April 5, 2025'
-    },
-  ];
+  lastTransaction: string = 'no transactions yet';
+  transactions: Array<any> = [];
+
+  constructor(private readonly walletService:WalletService , private readonly userService:UserService){}
+  ngOnInit(): void {
+    this.userService.getProfile().subscribe({
+      next:(res)=>{
+        const userId = res.data._id;
+        this.walletService.getTicketByUserId(userId).subscribe({
+          next:(res)=>{
+            const myWallet =res.data
+            this.balance = myWallet.balance;
+            this.currency = myWallet.currency
+            this.transactions = myWallet.transactions
+            this.lastTransaction = this.transactions[this.transactions.length-1].createdAt
+          },
+          error:(err)=>{
+            console.log(err.message)
+            throw new Error(err)
+          }
+        })
+      },
+      error:(err)=>{
+        console.log(err.message)
+        throw new Error(err)
+      }
+    })
+  }
 
   formatAmount(amount: number): string {
     return Math.abs(amount).toFixed(2);
--- src/app/services/wallet/wallet.service.ts
+++ src/app/services/wallet/wallet.service.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class WalletService{
-    
+    private apiUrl:string = "http://localhost:3000/wallet"
+    constructor(private readonly http:HttpClient){}
+
+    getTicketByUserId(userId:string):Observable<any>{
+        return this.http.get(`${this.apiUrl}/${userId}`)
+    }
 }
