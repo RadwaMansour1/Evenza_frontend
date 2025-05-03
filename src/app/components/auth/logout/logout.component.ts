e50290f2e2ef6f07a4de1a8714f9dabaec1db42b
Hassannzidan <hzidan014@gmail.com>
2025-05-03 23:32:53 +0300

    user profile

--- src/app/components/auth/logout/logout.component.ts
+++ src/app/components/auth/logout/logout.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { featherAlertCircle, featherLogOut, featherShield } from '@ng-icons/feather-icons';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth/auth.service';
 
 @Component({
   selector: 'app-logout',
-  imports: [],
+  imports: [NgIcon],
   templateUrl: './logout.component.html',
-  styleUrl: './logout.component.css'
+  styleUrl: './logout.component.css',
+  viewProviders:[provideIcons({featherShield,featherAlertCircle,featherLogOut})]
 })
 export class LogoutComponent {
 
+  constructor(private router: Router, private authService: AuthService){
+
+  }
+  logout(){
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
