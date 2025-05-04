ca5176d8dc4bcd1332393846e28239b251b084d4
Asmaa omar <119797320+AsmaaOmarr@users.noreply.github.com>
2025-05-04 19:35:40 +0300

    refactor: update spacing classes for RTL support across multiple components

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
     "./public/**/*.html",
-    './src/index.html', 
+    './src/index.html',
     './src/**/*.{html,ts}', // Include all HTML and TS files
 
   ],
@@ -14,8 +14,14 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
         poppins: ["Poppins", "sans-serif"],
       },
+      spacing: {
+        0.5: "0.125rem",
+        1.5: "0.375rem",
+        22: "5.5rem",
+        26: "6.5rem",
+      }
     },
-    
+
   },
   plugins: [],
 }
