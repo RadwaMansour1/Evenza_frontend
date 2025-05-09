1210addaa395898b8bb5ee348e2a4333242e458a
Radwa Mansour <radwa.mansour2024@gmail.com>
2025-05-09 21:07:25 +0300

    Prevent refunds within 2 days of event date

--- src/app/components/home/components/reviews-section/reviews-section.component.ts
+++ src/app/components/home/components/reviews-section/reviews-section.component.ts
@@ -10,6 +10,7 @@ import {
 import { ReviewService } from '../../../../services/review/review.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { LanguageService } from '../../../../services/language/language.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-reviews-section',
@@ -33,7 +34,9 @@ export class ReviewsSectionComponent implements OnInit {
 
   constructor(
     private reviewService: ReviewService,
-    private languageService: LanguageService
+    private languageService: LanguageService,
+    private sanitizer: DomSanitizer
+
   ) {
     const lang = this.languageService.getCurrentLanguage();
     this.currentLang = lang;
@@ -46,14 +49,33 @@ export class ReviewsSectionComponent implements OnInit {
   fetchReviews() {
     this.reviewService.getAllReviews().subscribe({
       next: (response) => {
-        this.reviews = response.data;
-        console.log('Reviews fetched:', this.reviews);
+        this.reviews = response.data.map((review: { userId: any; user: any; }) => {
+          const user = review.userId || review.user;
+          const avatarUrl = this.getAvatarUrl(user);
+          console.log('Avatar URL:', avatarUrl, 'User:', user);
+
+          return {
+            ...review,
+            user,
+            profileImageUrl: avatarUrl
+          };
+        });
       },
       error: (err) => {
         console.error('Failed to fetch reviews:', err);
-      },
+      }
     });
   }
+
+
+  getAvatarUrl(user: any): string {
+    if (user?.photo) return user.photo;
+
+    const fullName = `${user?.firstName || ''} ${user?.lastName || ''}`.trim();
+    const encoded = encodeURIComponent(fullName || 'User');
+    return `https://api.dicebear.com/7.x/initials/svg?seed=${encoded}`;
+  }
+
 
   scrollLeft(slider: HTMLElement) {
     slider.scrollBy({ left: -300, behavior: 'smooth' });
