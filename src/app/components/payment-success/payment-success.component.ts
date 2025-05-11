c43ce910c1148da62f0311a0f8a2b3e80cba5586
“Ahmed <ahmedmostafa4497@gmail.com>
2025-05-11 16:05:04 +0300

    update success page

--- src/app/components/payment-success/payment-success.component.ts
+++ src/app/components/payment-success/payment-success.component.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { TicketsService } from '../../services/tickets/tickets.service';
+import { UserService } from '../../services/profile/user.service';
 
 @Component({
   selector: 'app-payment-success',
@@ -12,18 +14,37 @@ import { TranslateModule } from '@ngx-translate/core';
 export class PaymentSuccessComponent implements OnInit {
   barcodeImageSrc: string | null = null;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private readonly ticketService:TicketsService,private readonly userService:UserService) {}
 
   async ngOnInit(): Promise<void> {
     const urlParams = new URLSearchParams(window.location.search);
     const sessionId = urlParams.get('session_id');
-    const orderId = urlParams.get('orderId');
-    const ticketId = urlParams.get('ticketId');
+    let ticketId = urlParams.get('ticketId');
     const paymentMethod = urlParams.get('payment_method') || 'stripe';
+
+    if(!ticketId){
+      this.userService.getProfile().subscribe({
+        next:(res)=>{
+          const userId = res.data._id;
+          this.ticketService.getTicketByUserId(userId).subscribe({
+            next:(res:any)=>{
+              const tickets = res.data;
+              console.log("user tickets ",tickets);
+              ticketId = tickets[tickets.length-1]._id;
+            },
+            error:(err)=>{
+              console.log(err)
+            }
+          })
+        },
+        error:(err)=>{
+          console.log(err)
+        }
+      })
+    }
 
     try {
       console.log({
-        orderId:orderId,
         tiketId:ticketId
       })
 
@@ -32,7 +53,7 @@ export class PaymentSuccessComponent implements OnInit {
       // :
       // `https://evenzabackend-production-2fb4.up.railway.app/payments/barcode/${orderId}/${ticketId}?payment_method=${paymentMethod}`;
 
-      const barcodeUrl =`https://evenzabackend-production-2fb4.up.railway.app/payments/barcode/${orderId}/${ticketId}?payment_method=${paymentMethod}`;
+      const barcodeUrl =`https://evenzabackend-production-2fb4.up.railway.app/payments/barcode/${ticketId}?payment_method=${paymentMethod}`;
 
 
       const blob = await this.http.get(barcodeUrl, { responseType: 'blob' }).toPromise();
